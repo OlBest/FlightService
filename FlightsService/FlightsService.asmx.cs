64495915f86f11787a4ce56962f2442b959c8358
NManichev <Nikita.Manichev@gmail.com>
2014-10-12 22:42:46 +0300

    Added payment service

--- FlightsService/FlightsService.asmx.cs
+++ FlightsService/FlightsService.asmx.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Services;
+using System.Web.UI.WebControls;
+using FlightsService.AccountService;
 using FlightsService.App_Data;
 using FlightsService.App_Data.FlightsDataSetTableAdapters;
+using FlightsService.PaymentService;
 
 namespace FlightsService
 {
@@ -16,134 +21,275 @@ namespace FlightsService
     // [System.Web.Script.Services.ScriptService]
     public class FlightsService : WebService
     {
+        private readonly AccountService.AccountServiceService _accountClient = new AccountServiceService();
+        private readonly PaymentService.PaymentServiceService _paymentClient = new PaymentServiceService();
+        
+        private readonly int _serviceId;
+        private Timer _timer;
+        private string _company;
+        private bool _isPayed;
 
-        [WebMethod]
-        public void BuyTicket(string departure, string arrival, string name, string surname)
+        public FlightsService()
         {
-            var adapter = new FLIGHTSTableAdapter();
-            adapter.InsertFlight(Convert.ToInt32(adapter.GetNumberFlights() + 1),
-                Convert.ToInt32(new FLIGHTSPRICETableAdapter().GetFlightsPriceId(departure, arrival)),
-                Convert.ToInt32(new CUSTOMERTableAdapter().GetCustomerIdByNameAndSurname(name, surname)));
+            _serviceId = _accountClient.register("FlightsService");
+            _isPayed = false;
+        }
+
+        private void PayedTimeEnded(object state)
+        {
+            _isPayed = _paymentClient.isPayed(_serviceId, _company);
         }
 
         [WebMethod]
-        public DataTable GetAllFlights()
+        public void RegisterCompany(string company)
         {
-            return new FlightsTableAdapter().GetAllFlights();
+            _company = company;
         }
 
         [WebMethod]
-        public int GetFlightPriceId(string departure, string arrival)
+        public void Pay()
         {
-            return Convert.ToInt32(new FLIGHTSPRICETableAdapter().GetFlightsPriceId(departure, arrival));
+            _paymentClient.pay(_serviceId, _company);
+            _isPayed = _paymentClient.isPayed(_serviceId, _company);
         }
 
         [WebMethod]
-        public void UpdateFlightPrice(int flightPriceId, string departure, string arrival, int price)
+        public  void PayForTimeInMilliSeconds(long time)
         {
-            new FLIGHTSPRICETableAdapter().UpdateFlightPrice(price, flightPriceId, departure, arrival);
+            _paymentClient.payForMilliseconds(_serviceId, _company, time);
+            _isPayed = _paymentClient.isPayed(_serviceId, _company);
+            _timer = new Timer(PayedTimeEnded,null,time,-1);
         }
 
         [WebMethod]
-        public DataTable GetFlightsPrice()
+        public bool BuyTicket(string departure, string arrival, string name, string surname)
         {
-            return new FlightsPriceTableAdapter().GetFligthsPrice();
+            if (_isPayed)
+            {
+                var adapter = new FLIGHTSTableAdapter();
+                adapter.InsertFlight(Convert.ToInt32(adapter.GetNumberFlights() + 1),
+                    Convert.ToInt32(new FLIGHTSPRICETableAdapter().GetFlightsPriceId(departure, arrival)),
+                    Convert.ToInt32(new CUSTOMERTableAdapter().GetCustomerIdByNameAndSurname(name, surname)));
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public void AddFlightPrice(string departure, string arrival, int price)
+        public bool GetAllFlights(out DataTable table)
         {
-            var adapter = new FLIGHTSPRICETableAdapter();
-            var cityAdapter = new CITIESTableAdapter();
-            adapter.InsertFlightPrice(Convert.ToInt32(adapter.GetNumberFlightPrice() + 1),
-                Convert.ToInt32(cityAdapter.GetCityId(departure)),Convert.ToInt32(cityAdapter.GetCityId(arrival)), price);
+            table = null;
+            if (_isPayed)
+            {
+                table = new FlightsTableAdapter().GetAllFlights();
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public void CancelTicket(int flightId)
+        public bool GetFlightPriceId(string departure, string arrival, out int id)
         {
-            new FLIGHTSTableAdapter().DeleteFlight(flightId);
+            id = -1;
+            if (_isPayed)
+            {
+                id = Convert.ToInt32(new FLIGHTSPRICETableAdapter().GetFlightsPriceId(departure, arrival));
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public void UpdateTicket(int flightId, int flightPriceId, string name, string surname)
+        public bool UpdateFlightPrice(int flightPriceId, string departure, string arrival, int price)
         {
-            var adapter = new FLIGHTSTableAdapter();
-            var customerAdapter = new CUSTOMERTableAdapter();
-            adapter.UpdateFlight(flightPriceId, Convert.ToInt32(customerAdapter.GetCustomerIdByNameAndSurname(name, surname)), flightId);
+            if (_isPayed)
+            {
+                new FLIGHTSPRICETableAdapter().UpdateFlightPrice(price, flightPriceId, departure, arrival);
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public void DeleteFlightPrice(string departure, string arrival)
+        public bool GetFlightsPrice(out DataTable table)
         {
-            var adapter = new FLIGHTSPRICETableAdapter();
-            adapter.DeleteFlightPrice(Convert.ToInt32(adapter.GetFlightsPriceId(departure, arrival)));
+            table = null;
+            if (_isPayed)
+            {
+                table = new FlightsPriceTableAdapter().GetFligthsPrice();
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public DataTable GetArrivalCitiesByDeparture (string departure)
+        public bool AddFlightPrice(string departure, string arrival, int price)
         {
-            return new CityTableAdapter().GetArrivalCitiesByDeparture(departure);
+            if (_isPayed)
+            {
+                var adapter = new FLIGHTSPRICETableAdapter();
+                var cityAdapter = new CITIESTableAdapter();
+                adapter.InsertFlightPrice(Convert.ToInt32(adapter.GetNumberFlightPrice() + 1),
+                    Convert.ToInt32(cityAdapter.GetCityId(departure)), Convert.ToInt32(cityAdapter.GetCityId(arrival)),
+                    price);
+                return true;
+            }
+            return false;
+        }
+
+        [WebMethod]
+        public bool CancelTicket(int flightId)
+        {
+            if (_isPayed)
+            {
+                new FLIGHTSTableAdapter().DeleteFlight(flightId);
+                return true;
+            }
+            return false;
+        }
+
+        [WebMethod]
+        public bool UpdateTicket(int flightId, int flightPriceId, string name, string surname)
+        {
+            if (_isPayed)
+            {
+                var adapter = new FLIGHTSTableAdapter();
+                var customerAdapter = new CUSTOMERTableAdapter();
+                adapter.UpdateFlight(flightPriceId,
+                    Convert.ToInt32(customerAdapter.GetCustomerIdByNameAndSurname(name, surname)), flightId);
+                return true;
+            }
+            return false;
+        }
+
+        [WebMethod]
+        public bool DeleteFlightPrice(string departure, string arrival)
+        {
+            if (_isPayed)
+            {
+                var adapter = new FLIGHTSPRICETableAdapter();
+                adapter.DeleteFlightPrice(Convert.ToInt32(adapter.GetFlightsPriceId(departure, arrival)));
+                return true;
+            }
+            return false;
+        }
+
+        [WebMethod]
+        public  bool GetArrivalCitiesByDeparture (string departure, out DataTable table)
+        {
+            table = null;
+            if (_isPayed)
+            {
+                table = new CityTableAdapter().GetArrivalCitiesByDeparture(departure);
+                return true;
+            }
+            return false;
         }
 
 
         [WebMethod]
-        public void AddCity(string city)
+        public bool AddCity(string city)
         {
-            var adapter = new CITIESTableAdapter();
-            adapter.AddCity(Convert.ToInt32(adapter.GetNumberCities() + 1), city);
+            if(_isPayed)
+            { 
+                var adapter = new CITIESTableAdapter();
+                adapter.AddCity(Convert.ToInt32(adapter.GetNumberCities() + 1), city);
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public void DeleteCity(string city)
+        public bool DeleteCity(string city)
         {
-            new CITIESTableAdapter().DeleteCity(city);
+            if(_isPayed)
+            { 
+                 new CITIESTableAdapter().DeleteCity(city);
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public void AddCustomer(string name,string surname)
+        public bool AddCustomer(string name,string surname)
         {
-            var adapter = new CUSTOMERTableAdapter();
-            adapter.AddCustomer(Convert.ToInt32(adapter.GetNumberCustomers()) + 1, name, surname);
+            if (_isPayed)
+            {
+                var adapter = new CUSTOMERTableAdapter();
+                adapter.AddCustomer(Convert.ToInt32(adapter.GetNumberCustomers()) + 1, name, surname);
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public DataTable GetCustomers()
+        public bool GetCustomers(out DataTable table)
         {
-            var adapter = new CustomersTableAdapter();
-            return adapter.GetCustomers();
+            table = null;
+            if(_isPayed)
+            { 
+                var adapter = new CustomersTableAdapter();
+                table = adapter.GetCustomers();
+                return true;
+            }
+            return false;
         }
 
         [WebMethod]
-        public DataTable GetCustomerFlights(string name , string surname)
+        public bool GetCustomerFlights(string name , string surname,out DataTable table)
         {
-            var adapter = new CustomerFlightsTableAdapter();
-            return adapter.GetCustomerFlights(Convert.ToInt32(new CUSTOMERTableAdapter().GetCustomerIdByNameAndSurname(name, surname)));
+            table = null;
+            if (_isPayed)
+            {
+                var adapter = new CustomerFlightsTableAdapter();
+                table =
+                    adapter.GetCustomerFlights(
+                        Convert.ToInt32(new CUSTOMERTableAdapter().GetCustomerIdByNameAndSurname(name, surname)));
+                return true;
+            }
+            return false;
         }
 
 
 
 
         [WebMethod]
-        public void UpdateCustomer(string newName, string newSurname,string oldName,string oldSurname)
+        public bool UpdateCustomer(string newName, string newSurname,string oldName,string oldSurname)
         {
-            new CUSTOMERTableAdapter().UpdateCustomer(newName, newSurname,
-               Convert.ToInt32(new CUSTOMERTableAdapter().GetCustomerIdByNameAndSurname(oldName, oldSurname)));
+            if (_isPayed)
+            {
+                new CUSTOMERTableAdapter().UpdateCustomer(newName, newSurname,
+                    Convert.ToInt32(new CUSTOMERTableAdapter().GetCustomerIdByNameAndSurname(oldName, oldSurname)));
+                return true;
+            }
+            return false;
         }
 
 
         [WebMethod]
-        public DataTable GetFlightsCities()
+        public bool GetFlightsCities(out DataTable table)
         {
-            var adater = new CityTableAdapter();
-            return adater.GetFlightCities();
+            table = null;
+            if (_isPayed)
+            {
+                var adater = new CityTableAdapter();
+                table = adater.GetFlightCities();
+                return true;
+            }
+            return false;
         }
 
 
         [WebMethod]
-        public int GetFlightPrice(string departure, string arrival)
+        public bool GetFlightPrice(string departure, string arrival, out int price)
         {
-            return Convert.ToInt32(new FLIGHTSPRICETableAdapter().GetFlightPrice(departure, arrival));
+            price = -1;
+            if (_isPayed)
+            {
+                price = Convert.ToInt32(new FLIGHTSPRICETableAdapter().GetFlightPrice(departure, arrival));
+                return true;
+            }
+            return false;
         }
     }
 }
